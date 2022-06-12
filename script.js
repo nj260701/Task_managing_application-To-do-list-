576a56ee2f51a5dafb6dfe4ff104cd8000b439ac
nj260701 <neeraj.nj.260701@gmail.com>
2022-06-12 23:31:00 +0530

    Ticket task functionality added

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let addButton = document.querySelector(".add");
 let modalContainer= document.querySelector(".modal-container")
+let mainContainer = document.querySelector('.main-cont')
+let taskArea = document.querySelector('.textArea-container')
 
 
 // modalContainer.style.display = "none"
@@ -18,5 +20,34 @@ addButton.addEventListener('click', function(){
     else{
         modalContainer.style.display = "none"
     }
-
 })
+
+modalContainer.addEventListener("keydown", function(e){
+    let key= e.key
+
+    if(key=='Shift'){
+        createTicket(taskArea.value)
+        modalContainer.style.display = 'none'
+        addFlag=false
+        taskArea.value=''
+    }
+})
+
+
+function createTicket(ticketTask){
+    // let id = shortid()
+    let tickerContainer = document.createElement('div')
+    tickerContainer.setAttribute('class','ticket-cont')
+
+    tickerContainer.innerHTML = `
+    <div class="ticket-color"></div>
+            <div class="ticket-id">id1</div>
+            <div class="task-area">${ticketTask}</div>
+            <div class="ticket-lock">
+                <i class="fa-solid fa-lock"></i>
+            </div>`
+
+    mainContainer.append(tickerContainer)
+
+}
+
